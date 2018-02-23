b8f6e02f17db5a4bfa3064777f08f1f13114ab22
Matthias Schneider <ms@wck.biz>
2018-02-23 16:57:01 +0100

    Added support for ENV Config Variables
    APP_PORT
    ETCD_ROOT_KEY
    ETCD_ADDR
    ETCD_USERNAME
    ETCD_PASSWORD

--- conf/config.go
+++ conf/config.go
@@ -17,6 +17,22 @@ type Config struct {
 	CAFile        string
 }
 
+func getEnv(key, defaultValue string) string {
+    value := os.Getenv(key)
+    if len(value) == 0 {
+        return defaultValue
+    }
+    return value
+}
+
+func getEnvStrList(key string, defaultValue string) []string {
+    value := os.Getenv(key)
+    if len(value) == 0 {
+        return strings.Split(defaultValue, ",")
+    }
+    return strings.Split(value, ",")
+}
+
 func Init(filepath string) (*Config, error) {
 	cfg, err := ini.Load(filepath)
 	if err != nil {
@@ -25,19 +41,19 @@ func Init(filepath string) (*Config, error) {
 
 	c := &Config{}
 
-	appSec := cfg.Section("app")
-	c.Port = appSec.Key("port").Value()
-	c.Auth = appSec.Key("auth").MustBool()
+        appSec := cfg.Section("app")
+        c.Port = getEnv("APP_PORT", appSec.Key("port").Value())
+        c.Auth = appSec.Key("auth").MustBool()
 
-	etcdSec := cfg.Section("etcd")
-	c.EtcdRootKey = etcdSec.Key("root_key").Value()
-	c.DirValue = etcdSec.Key("dir_value").Value()
-	c.EtcdEndPoints = etcdSec.Key("addr").Strings(",")
-	c.EtcdUsername = etcdSec.Key("username").Value()
-	c.EtcdPassword = etcdSec.Key("password").Value()
-	c.CertFile = etcdSec.Key("cert_file").Value()
-	c.KeyFile = etcdSec.Key("key_file").Value()
-	c.CAFile = etcdSec.Key("ca_file").Value()
+        etcdSec := cfg.Section("etcd")
+        c.EtcdRootKey = getEnv("ETCD_ROOT_KEY", etcdSec.Key("root_key").Value())
+        c.DirValue = etcdSec.Key("dir_value").Value()
+        c.EtcdEndPoints = getEnvStrList("ETCD_ADDR", etcdSec.Key("addr").Value())
+        c.EtcdUsername = getEnv("ETCD_USERNAME", etcdSec.Key("username").Value())
+        c.EtcdPassword = getEnv("ETCD_PASSWORD", etcdSec.Key("password").Value())
+        c.CertFile = etcdSec.Key("cert_file").Value()
+        c.KeyFile = etcdSec.Key("key_file").Value()
+        c.CAFile = etcdSec.Key("ca_file").Value()
 
 	return c, nil
 }
